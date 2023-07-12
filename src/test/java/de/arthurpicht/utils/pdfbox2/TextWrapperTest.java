5777b2aa6b714750e91cb214c0e65c505eb64302
Fabian Kalk <fabian.kalk@comrize.de>
2023-07-12 15:57:14 +0200

    +updated test-cases

--- src/test/java/de/arthurpicht/utils/pdfbox2/TextWrapperTest.java
+++ src/test/java/de/arthurpicht/utils/pdfbox2/TextWrapperTest.java
@@ -44,45 +44,79 @@ class TextWrapperTest {
     }
 
     @Test
-    public void whitespace() throws UtilsPdfbox2Exception {
-
+    public void plausibilityLongWords() throws UtilsPdfbox2Exception {
         TextWrapperConfig textWrapperConfig = new TextWrapperConfig(
                 PDType1Font.COURIER,
                 12,
                 100);
 
-        String text = "    Lorem                             ipsum    dolor sit amet, consetetur sadipscing elitr," +
-                " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua." +
-                " At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata" +
-                " sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr," +
-                " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua." +
-                " At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata" +
-                " sanctus est Lorem ipsum dolor sit amet.";
+        String text = "Lorem_ipsum dolor_sit_amet, consetetur_sadipscing_elitr, sed diam nonumy eirmod tempor" +
+                " invidunt ut labore et dolore magna aliquyam erat";
 
         List<String> wrappedText = TextWrapper.wrap(textWrapperConfig, text);
 
+        List<String> expected = Arrays.asList(
+                "Lorem_ipsum",
+                "dolor_sit_ame",
+                "t,",
+                "consetetur_sa",
+                "dipscing_elit",
+                "r, sed diam",
+                "nonumy eirmod",
+                "tempor",
+                "invidunt ut",
+                "labore et",
+                "dolore magna",
+                "aliquyam erat"
+        );
+
         wrappedText.forEach(System.out::println);
+
+        assertEquals(expected, wrappedText);
     }
 
     @Test
-    public void longWords() throws UtilsPdfbox2Exception {
-
+    public void plausibilityWhitespace() throws UtilsPdfbox2Exception {
         TextWrapperConfig textWrapperConfig = new TextWrapperConfig(
                 PDType1Font.COURIER,
                 12,
                 100);
 
-        String text = " Lorem ipsum    dolor_sit_amet,_consetetur_sadipscing_elitr, sed diam nonumy eirmod tempor" +
-                " invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et" +
-                " justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum" +
-                " dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod" +
-                " tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam" +
-                " et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem" +
-                " ipsum dolor sit amet.";
+        String text = "Lorem       ipsum               dolor                 sit               amet,                " +
+                "consetetur               sadipscing                 elitr,                  sed                    " +
+                "diam              nonumy                 eirmod                  tempor                            " +
+                "invidunt                ut                  labore                 et            dolore            " +
+                "magna                 aliquyam                  erat";
 
         List<String> wrappedText = TextWrapper.wrap(textWrapperConfig, text);
 
+        List<String> expected = Arrays.asList(
+                "Lorem",
+                "ipsum",
+                "dolor",
+                "sit",
+                "amet,",
+                "consetetur",
+                "sadipscing",
+                "elitr,",
+                "sed",
+                "diam",
+                "nonumy",
+                "eirmod",
+                "tempor",
+                "invidunt",
+                "ut",
+                "labore",
+                "et",
+                "dolore",
+                "magna",
+                "aliquyam",
+                "erat"
+        );
+
         wrappedText.forEach(System.out::println);
+
+        assertEquals(expected, wrappedText);
     }
 
 }
